92b948a9aa51875ef26a5eb77ad7c9a0d97400b4
Matt Pants <nostickgnostic@gmail.com>
2013-04-03 10:56:59 -0400

    Added class search to main page

--- learningcommons/urls.py
+++ learningcommons/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from ourschool.views import display_meta_t
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,13 +6,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^meta/$', display_meta_t),
+    url(r'^meta/$', 'ourschool.views.display_meta_t', name='meta'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'ourschool.views.home', name='home'),
+    url(r'^test', 'ourschool.views.test', name='test'),
+    url(r'^search/$','ourschool.views.search',name='search'),
     # url(r'^learningcommons/', include('learningcommons.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
--- ourschool/views.py
+++ ourschool/views.py
@@ -11,11 +11,24 @@ def display_meta_t(request):
   test = 'Meta'
   c = Context({'values': values,'testvar':test})
   t = Template('{{ testvar }}<br/>{% for item in values %} {{ item }}<br/>{% endfor %}')
-  return HttpResponse(t.render(c))
+  return render_to_response(t,c)
+
+def test(request):
+  return HttpResponse('Welcome to page %s' % request.path)
+
 
 def home(request):
   #Home page
-  classes = source.objects.all()
-  c = RequestContext(request, {'classes':classes})
-  return render_to_response('home.html',c)
+  if 'q' in request.GET and request.GET['q']:
+    q = request.GET['q']
+    sources = source.objects.filter(classtitle__icontains=q)
+    return render_to_response('search_results.html', {'sources': sources, 'query': q})
+  elif 'q' in request.GET and request.GET['q'] == '':
+    classes = source.objects.all()
+    c = RequestContext(request, {'classes':classes,'error':True})
+    return render_to_response('home.html',c)
+  else:
+    classes = source.objects.all()
+    c = RequestContext(request, {'classes':classes})
+    return render_to_response('home.html',c)
   
