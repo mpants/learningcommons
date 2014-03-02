f6c0330a427fe9b5a977672ad01c636e76f30349
Matt Pants <nostickgnostic@gmail.com>
2014-03-02 11:35:25 -0500

    change database location in settings

--- learningcommons/settings.py
+++ learningcommons/settings.py
@@ -3,6 +3,7 @@ import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 ADMINS = (
     # ('Matt', 'nostickgnostic@gmail.com'),
@@ -13,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lc.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH,'lc.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
